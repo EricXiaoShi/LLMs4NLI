7c652b4e99614f27cfd12fe7f9129df9cea65e11
EricXiaoShi <46922408+EricXiaoShi@users.noreply.github.com>
2024-01-06 21:16:40 +0800

    Create BERT_finetuned.py

---
+++ BERT_finetuned.py
@@ -0,0 +1,107 @@
+import json
+import multiprocessing
+import os
+from mxnet import gluon, np, npx
+from mxnet.gluon import nn
+from d2l import mxnet as d2l
+
+npx.set_np()
+
+def load_pretrained_model(pretrained_model, num_hiddens, ffn_num_hiddens,
+                          num_heads, num_layers, dropout, max_len, devices):
+    data_dir = d2l.download_extract(pretrained_model)
+    vocab = d2l.Vocab()
+    vocab.idx_to_token = json.load(open(os.path.join(data_dir,
+         'vocab.json')))
+    vocab.token_to_idx = {token: idx for idx, token in enumerate(
+        vocab.idx_to_token)}
+    bert = d2l.BERTModel(len(vocab), num_hiddens, ffn_num_hiddens,
+                         num_heads, num_layers, dropout, max_len)
+    bert.load_parameters(os.path.join(data_dir, 'pretrained.params'),
+                         ctx=devices)
+    return bert, vocab
+
+
+devices = d2l.try_all_gpus()
+bert, vocab = load_pretrained_model(
+    'bert.base', num_hiddens=256, ffn_num_hiddens=512, num_heads=4,
+    num_layers=2, dropout=0.1, max_len=512, devices=devices)
+
+class SNLIBERTDataset(gluon.data.Dataset):
+    def __init__(self, dataset, max_len, vocab=None):
+        all_premise_hypothesis_tokens = [[
+            p_tokens, h_tokens] for p_tokens, h_tokens in zip(
+            *[d2l.tokenize([s.lower() for s in sentences])
+              for sentences in dataset[:2]])]
+
+        self.labels = np.array(dataset[2])
+        self.vocab = vocab
+        self.max_len = max_len
+        (self.all_token_ids, self.all_segments,
+         self.valid_lens) = self._preprocess(all_premise_hypothesis_tokens)
+        print('read ' + str(len(self.all_token_ids)) + ' examples')
+
+    def _preprocess(self, all_premise_hypothesis_tokens):
+        pool = multiprocessing.Pool(4)  # 使用4个进程
+        out = pool.map(self._mp_worker, all_premise_hypothesis_tokens)
+        all_token_ids = [
+            token_ids for token_ids, segments, valid_len in out]
+        all_segments = [segments for token_ids, segments, valid_len in out]
+        valid_lens = [valid_len for token_ids, segments, valid_len in out]
+        return (np.array(all_token_ids, dtype='int32'),
+                np.array(all_segments, dtype='int32'),
+                np.array(valid_lens))
+
+    def _mp_worker(self, premise_hypothesis_tokens):
+        p_tokens, h_tokens = premise_hypothesis_tokens
+        self._truncate_pair_of_tokens(p_tokens, h_tokens)
+        tokens, segments = d2l.get_tokens_and_segments(p_tokens, h_tokens)
+        token_ids = self.vocab[tokens] + [self.vocab['<pad>']] \
+                             * (self.max_len - len(tokens))
+        segments = segments + [0] * (self.max_len - len(segments))
+        valid_len = len(tokens)
+        return token_ids, segments, valid_len
+
+    def _truncate_pair_of_tokens(self, p_tokens, h_tokens):
+        while len(p_tokens) + len(h_tokens) > self.max_len - 3:
+            if len(p_tokens) > len(h_tokens):
+                p_tokens.pop()
+            else:
+                h_tokens.pop()
+
+    def __getitem__(self, idx):
+        return (self.all_token_ids[idx], self.all_segments[idx],
+                self.valid_lens[idx]), self.labels[idx]
+
+    def __len__(self):
+        return len(self.all_token_ids)
+
+batch_size, max_len, num_workers = 512, 128, d2l.get_dataloader_workers()
+data_dir = d2l.download_extract('SNLI')
+train_set = SNLIBERTDataset(d2l.read_snli(data_dir, True), max_len, vocab)
+test_set = SNLIBERTDataset(d2l.read_snli(data_dir, False), max_len, vocab)
+train_iter = gluon.data.DataLoader(train_set, batch_size, shuffle=True,
+                                   num_workers=num_workers)
+test_iter = gluon.data.DataLoader(test_set, batch_size,
+                                  num_workers=num_workers)
+
+class BERTClassifier(nn.Block):
+    def __init__(self, bert):
+        super(BERTClassifier, self).__init__()
+        self.encoder = bert.encoder
+        self.hidden = bert.hidden
+        self.output = nn.Dense(3)
+
+    def forward(self, inputs):
+        tokens_X, segments_X, valid_lens_x = inputs
+        encoded_X = self.encoder(tokens_X, segments_X, valid_lens_x)
+        return self.output(self.hidden(encoded_X[:, 0, :]))
+
+net = BERTClassifier(bert)
+net.output.initialize(ctx=devices)
+
+lr, num_epochs = 1e-4, 5
+trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': lr})
+loss = gluon.loss.SoftmaxCrossEntropyLoss()
+d2l.train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs,
+    devices, d2l.split_batch_multi_inputs)
